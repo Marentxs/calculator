0e54de53f0dc45e65d1c9b6e6bf9f33e34d89c19
Julian <111262742+Marentxs@users.noreply.github.com>
2025-10-01 18:04:36 -0500

    add operators functionality, answer carries on to next operation

--- javascript.js
+++ javascript.js
@@ -1,9 +1,83 @@
 const display = document.getElementById("display");
+const numberButtons = document.querySelectorAll(".number");
+const operatorButtons = document.querySelectorAll(".operator");
+const equalsButton = document.getElementById("equals");
+const clearButton = document.getElementById("clear");
+
+let firstNumber = "";
+let operator = "";
+let secondNumber = "";
 
 function appendToDisplay(value) {
   display.value += value;
 }
 
 function clearDisplay() {
+  firstNumber = "";
+  operator = "";
+  secondNumber = "";
   display.value = "";
 }
+
+function add(a, b) {
+  return a + b;
+}
+
+function substract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function calculate() {
+  let result;
+  let a = Number(firstNumber);
+  let b = Number(secondNumber);
+
+  if (operator === "+") {
+    result = add(a, b);
+  } else if (operator === "-") {
+    result = substract(a, b);
+  } else if (operator === "*") {
+    result = multiply(a, b);
+  } else if (operator === "/") {
+    result = divide(a, b);
+  }
+
+  display.value = result;
+  firstNumber = result;
+  operator = "";
+  secondNumber = "";
+}
+
+function input(value, type) {
+  if (type === "operator") {
+    operator = value;
+  } else if (operator === "") {
+    firstNumber += value;
+  } else {
+    secondNumber += value;
+  }
+  display.value = firstNumber + operator + secondNumber;
+}
+
+numberButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    input(button.value, "number");
+  });
+});
+
+operatorButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    input(button.value, "operator");
+  });
+});
+
+equalsButton.addEventListener("click", calculate);
+clearButton.addEventListener("click", clearDisplay);
